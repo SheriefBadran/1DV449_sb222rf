05397911c5b1fae4cd41b6e68119fb09bd66bdfe
SheriefBadran <sheriefbadran@vwifi-weblogin-1003.lnu.se>
2014-12-04 19:40:48 +0100

    Click events on traffic message list trigger marker info window.

--- laboration3/mashUp/app/js/app.js
+++ laboration3/mashUp/app/js/app.js
@@ -3,11 +3,12 @@
  */
 window.onload = init;
 
-function init () {
+function init() {
 
+    renderList();
     var socket = io.connect('http://localhost:8000');
 
-    socket.on('load', function(data) {
+    socket.on('load', function (data) {
 
         socket.emit('test', {my: 'sending news from client'});
 
@@ -18,9 +19,12 @@ function init () {
 
         var selectList = document.querySelector('#select-list select');
 
-        selectList.addEventListener('change', function(e) {
+        selectList.addEventListener('change', function (e) {
 
-            switch(e.target.options.selectedIndex) {
+            renderer.trafficMessageList.textContent = '';
+
+            // 1. Clear array.
+            switch (e.target.options.selectedIndex) {
 
                 case 0:
                     renderer.createMarkers(renderer.arrangeData(renderer.roadData));
@@ -39,16 +43,11 @@ function init () {
                     break;
             }
         }, false);
-        //renderer.roadData = [];
-        //renderer.publicTransData = [];
-        //renderer.disruptionData = [];
-        //renderer.other = [];
-        //renderer.markerData = [];
-        // 1. Get selected from list with dom operation.
-        // 2. Switch on selected.
-        // 3. In switch case - renderMarkers belonging to category.
-        renderer.createMarkers(renderer.arrangeData(renderer.other));
+
+        renderer.createMarkers(renderer.arrangeData(renderer.markerData));
+        renderList(renderer.arrangeData(renderer.markerData));
     });
+
 
     var map;
     var renderer = {
@@ -59,36 +58,61 @@ function init () {
         disruptionData: [],
         other: [],
         markerData: [],
+        trafficMessageList: document.querySelector('#traffic-message-list'),
         addMarker: function () {
 
         },
         createMarkers: function (markerData) {
 
-            renderer.markers.forEach(function(marker) {
+            renderer.markers.forEach(function (marker) {
                 marker.setMap(null);
             });
 
-            markerData.forEach(function(markerObj, i) {
+            var prevClickedMarker;
+            var li;
+            markerData.forEach(function (markerObj, i) {
+
+                var infoMarkup = new infoWindowContent(markerObj);
+                var markup = infoMarkup.getInfoMarkup();
 
                 var markerCoordinate = new google.maps.LatLng(markerObj.latitude, markerObj.longitude);
                 var marker = new google.maps.Marker({
                     position: markerCoordinate,
                     map: map,
-                    title: markerObj.title
+                    title: markerObj.title,
+                    infoWindow: new google.maps.InfoWindow({
+                        content: markup
+                    })
                 });
 
                 renderer.markers.push(marker);
 
-                google.maps.event.addListener(marker, 'click', function() {
+                google.maps.event.addListener(marker, 'click', function () {
+
+                    if (prevClickedMarker) {
+
+                        prevClickedMarker.infoWindow.close();
+                    }
 
                     map.setZoom(6);
                     map.setCenter(markerCoordinate);
+                    marker.infoWindow.open(map, marker);
+
+                    prevClickedMarker = marker;
+
+                });
+                li = renderList(marker);
+                google.maps.event.addDomListener(li, 'click', function(e) {
+
+                    // Prevent a href action to be called.
+                    e.preventDefault();
+                    google.maps.event.trigger(marker, 'click');
                 });
             });
         },
-        getRelevantArray: function(array) {
+        getRelevantArray: function (array) {
 
-            var relevantArray = array.map(function(message, i) {
+            var relevantArray = array.map(function (message, i) {
 
                 //console.log(trafficMessage);
                 return {
@@ -97,21 +121,22 @@ function init () {
                     title: message.title,
                     description: message.description,
                     createddate: message.createddate,
-                    category: message.category
+                    category: message.category,
+                    subcategory: message.subcategory
                 };
             });
 
             return relevantArray;
         },
-        arrangeData: function(array) {
+        arrangeData: function (array) {
 
             return array.reverse().slice(0, 100);
         },
-        populateCategoryArrays: function(array) {
+        populateCategoryArrays: function (array) {
 
-            array.forEach(function(trafficMessage) {
+            array.forEach(function (trafficMessage) {
 
-                switch(trafficMessage['category']) {
+                switch (trafficMessage['category']) {
                     case 0:
                         renderer.roadData.push(trafficMessage);
                         break;
@@ -128,7 +153,6 @@ function init () {
                         break;
                 }
             });
-            //renderer.createMarkers(renderer.arrangeData(renderer.other));
         }
     };
 
@@ -149,3 +173,27 @@ function createMarkerArray() {
 
     return markerArray;
 }
+
+
+var renderList = function() {
+
+    // Initial one time procedure
+    var doc = document;
+    var ul = doc.querySelector('#traffic-message-list');
+    var liOrigin = doc.createElement('li');
+    var aOrigin = doc.createElement('a');
+    var li;
+    var a;
+
+    renderList = function(marker) {
+
+        li = liOrigin.cloneNode(true);
+        a = aOrigin.cloneNode(true);
+        a.setAttribute('href', '');
+        a.textContent = marker.title;
+        li.appendChild(a);
+        ul.appendChild(li);
+
+        return li;
+    };
+};
